e09e0d6d3e937aa06956361cf19a49c8f48f9360
haideralibd <haider4uiu@gmail.com>
2025-04-15 16:35:47 +0600

    move the doc manager components to free version

--- includes/Assets.php
+++ includes/Assets.php
@@ -60,6 +60,7 @@ class Assets {
                 array(
                     'adminUrl'     => admin_url(),
                     'hasManageCap' => current_user_can( 'manage_options' ),
+                    'weDocsUrl'    => admin_url( 'admin.php?page=wedocs#/' ),
                 ),
             );
         }
